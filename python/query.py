65bb650f2fc434d9d697f19c4ab5e5f9b1da4e34
sincerefly <kissbug8720@gmail.com>
2018-04-25 15:10:26 +0800

    update code use json instead of pickle

--- python/query.py
+++ python/query.py
@@ -1,37 +1,41 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
-import pickle
+from sys import platform
+import json
 
-pickle_file = open('data.pkl', 'rb')
-city = pickle.load(pickle_file)
+with open('data.json', 'r') as json_data:
+    city = json.load(json_data)
 
-idcard_number = str(input('请输入身份证号码：'))
-i = idcard_number
-cid = idcard_number[:6]
-birth = idcard_number[6:14]
-cname = city[cid]
+def convert_encode(string):
 
-num = int(i[1])
-tmpsum = int(i[0])*7 + int(i[1])*9 + int(i[2])*10 + int(i[3])*5 + int(i[4])*8 \
-        + int(i[5])*4 + int(i[6])*2 + int(i[7])*1 + int(i[8])*6 + int(i[9])*3 \
-        + int(i[10])*7 + int(i[11])*9 + int(i[12])*10 + int(i[13])*5 \
-        + int(i[14])*8 + int(i[15])*4 + int(i[16])*2;
+    ''' Windows 输出需转换编码
+    '''
+    return string if platform != "win32" else string.decode('utf-8').encode('gbk')
 
-remainder = tmpsum % 11
+idcard = str(input(convert_encode('请输入身份证号码：')))
+
+# 城市编码, 出生日期, 归属地 
+city_id = idcard[:6]
+birth = idcard[6:14]
+city_name = city[city_id].encode('utf-8')
+
+# 根据规则校验身份证是否符合规则
+idcard_tuple = [int(num) for num in list(idcard[:-1])]
+coefficient = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
+sum_value = sum([idcard_tuple[i] * coefficient[i] for i in range(17)])
+
+remainder = sum_value % 11
 
 maptable = {0: '1', 1: '0', 2: 'x', 3: '9', 4: '8', 5: '7', 6: '6', 7: '5', 8: '4', 9: '3', 10: '2'}
-if maptable[remainder] == i[17]:
-    print('<身份证合法>')
-
-    sex = int(i[16]) % 2
+if maptable[remainder] == idcard[17]:
+    print(convert_encode('<身份证合法>'))
+    sex = int(idcard[16]) % 2
     sex = '男' if sex == 1 else '女'
-    print('性别：' + sex)
-
-    print('出生日期:' + birth)
-    print('归属地:' + cname)
+    print(convert_encode('性别：' + sex))
+    print(convert_encode('出生日期:') + birth)
+    print(convert_encode('归属地:' + city_name))
 else :
-    print('<身份证不合法>')
-
+    print(convert_encode('<身份证不合法>'))
 
 
